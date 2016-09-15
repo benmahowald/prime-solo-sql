5736c8db5c60caeb9f227b239dd7f87530ec94e1
Ben Mahowald <ben.mahowald@gmail.com>
2016-09-14 20:17:02 -0500

    first commit

---
+++ solutions.sql
@@ -0,0 +1,27 @@
+—1.
+SELECT * FROM syntax_practice WHERE city='chicago';
+
+—2.
+SELECT * FROM syntax_practice WHERE username LIKE '%a%';
+
+—3.
+UPDATE syntax_practice SET account_balance='0.00', transactions_completed='0';
+
+UPDATE syntax_practice SET account_balance='10.00';
+
+—4.
+
+
+—5.
+
+
+—6.
+
+
+—7.
+
+
+—8.
+
+
+—9.
